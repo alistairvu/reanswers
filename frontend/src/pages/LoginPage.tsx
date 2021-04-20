5a72caad1b862482d80ca8737e4c6675e5c96df6
Alistair Vu <alistairvu@gmail.com>
2021-04-20 20:35:03 +0700

    Improve styles

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -13,16 +13,18 @@ const LoginPage: React.FC = () => {
       <Container className="mt-3">
         <Row>
           <Col className="d-none d-md-block mt-md-5" md={8}>
-            <h1>Welcome back to the land of curiosity.</h1>
+            <h1 className="fw-bold">Welcome back to the land of curiosity.</h1>
             <h2>
               Ask the community. Answer with your knowledge. Connect with other
               curious minds.
             </h2>
           </Col>
           <Col xs={12} md={4}>
-            <Card>
+            <Card className="shadow">
               <Card.Body>
-                <Card.Title>Log In</Card.Title>
+                <Card.Title className="fs-2 fw-bold text-center mb-3">
+                  Log In
+                </Card.Title>
                 <AuthLogInForm />
               </Card.Body>
             </Card>
