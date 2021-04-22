dfe1fb0b4284f9230d0d9ffd1e4d2a36d08ff278
phuonghaa <phuonggha39@gmail.com>
2021-04-22 17:30:30 +0700

    add showQuestion & deleteQuestion api

--- backend/modules/question/question.controller.ts
+++ backend/modules/question/question.controller.ts
@@ -2,9 +2,34 @@ import Question from "./question.model";
 import HTTPError from "../../httpError";
 import { Request, Response, NextFunction } from "express";
 
-// GET (all)
+// GET /api/questions
 
-// GET (one)
+// GET /api/questions/:id
+export const showQuestion = async (req: Request, res: Response, next: any) => {
+  try {
+    const question = await Question.findById(req.params.id)
+      .select("-__v")
+      .populate("author", "username");
+    // .populate({
+    //   path: "likedBy",
+    //   populate: { path: "author", select: "username" },
+    //   select: "-__v",
+    // })
+    // .populate({
+    //     path: "comments",
+    //     populate: { path: "comment", select: "username content" },
+    //     select: "-__v",
+    //   })
+
+    if (!question) {
+      throw new HTTPError("No matching questions found", 404);
+    }
+
+    res.send({ success: 1, data: question });
+  } catch (err) {
+    next(err);
+  }
+};
 
 //POST /api/questions/create
 export const createQuestion = async (
@@ -13,16 +38,14 @@ export const createQuestion = async (
   next: any
 ) => {
   try {
-    console.log(req.user._id);
-
     const { title, body, imageUrl, tags } = req.body;
-    const author = req.user._id
+    const author = req.user._id;
     const question = await Question.create({
       title,
       body,
       imageUrl,
       tags,
-      author
+      author,
     });
 
     res.send({ success: 1, data: question });
@@ -32,20 +55,26 @@ export const createQuestion = async (
 };
 
 // DELETE /api/questions/:id
-export const deleteQuestion = async (req: Request, res: Response, next: any) => {
-    try {
-      const question = await Question.findByIdAndDelete(req.params.id)
-  
-      if (!question) {
-        throw new HTTPError("No matching quetions found", 404)
-      }
-  
-      if (question.author.toString() !== req.user._id.toString()) {
-        throw new HTTPError("Action not allowed", 401)
-      }
-  
-      res.send({ success: 1, deleted: 1 })
-    } catch (err) {
-      next(err)
+export const deleteQuestion = async (
+  req: Request,
+  res: Response,
+  next: any
+) => {
+  try {
+    const question = await Question.findByIdAndDelete(req.params.id);
+    console.log(req.params.id);
+    console.log(question);
+
+    if (!question) {
+      throw new HTTPError("No matching quetions found", 404);
     }
+
+    if (question.author.toString() !== req.user._id.toString()) {
+      throw new HTTPError("Action not allowed", 401);
+    }
+
+    res.send({ success: 1, deleted: 1 });
+  } catch (err) {
+    next(err);
   }
+};
