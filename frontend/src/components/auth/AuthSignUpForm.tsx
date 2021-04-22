a94d68760b674cde6c5e63fe0ce8d7d8f4593738
Alistair Vu <alistairvu@gmail.com>
2021-04-22 16:06:48 +0700

    Remove unused variable

--- frontend/src/components/auth/AuthSignUpForm.tsx
+++ frontend/src/components/auth/AuthSignUpForm.tsx
@@ -1,57 +1,49 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Alert from 'react-bootstrap/Alert'
-import { useForm } from "react-hook-form";
-import axiosClient from "../../api";
-import UserContext from "../../context/userContext";
-import { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import Alert from "react-bootstrap/Alert"
+import { useForm } from "react-hook-form"
+import axiosClient from "../../api"
+import UserContext from "../../context/userContext"
+import { useContext, useState } from "react"
+import { useHistory } from "react-router-dom"
 
 interface SignUpInterface {
-  username: string;
-  email: string;
-  password: string;
-  passwordConfirmation: string;
+  username: string
+  email: string
+  password: string
+  passwordConfirmation: string
 }
 
 const AuthSignUpForm: React.FC = () => {
-  const [signUpError, setSignUpError] = useState<string>("");
+  const [signUpError, setSignUpError] = useState<string>("")
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SignUpInterface>()
-  const { setUser } = useContext(UserContext);
-  const history = useHistory();
+  const { setUser } = useContext(UserContext)
+  const history = useHistory()
 
   const handleSignUp = async (signUpData: SignUpInterface) => {
-    setSignUpError("");
+    setSignUpError("")
     if (signUpData.password !== signUpData.passwordConfirmation) {
-      setSignUpError("Passwords do not match!");
-      return;
+      setSignUpError("Passwords do not match!")
+      return
     }
 
     try {
-      const body = {
-        user: {
-          username: signUpData.username,
-          email: signUpData.email,
-          password: signUpData.password,
-        },
-      };
-      console.log(signUpData);
-      const { data } = await axiosClient.post("/api/auth/signup", signUpData);
+      const { data } = await axiosClient.post("/api/auth/signup", signUpData)
       if (data.success) {
-        console.log(data);
-        window.localStorage.setItem("jwt", data.token);
-        setUser(data.user);
-        history.push("/");
+        console.log(data)
+        window.localStorage.setItem("jwt", data.token)
+        setUser(data.user)
+        history.push("/")
       }
     } catch (err) {
-      console.log(err);
-      setSignUpError(err.response.data.message);
+      console.log(err)
+      setSignUpError(err.response.data.message)
     }
-  };
+  }
 
   return (
     <Form onSubmit={handleSubmit(handleSignUp)}>
@@ -102,7 +94,7 @@ const AuthSignUpForm: React.FC = () => {
         Sign Up
       </Button>
     </Form>
-  );
-};
+  )
+}
 
-export default AuthSignUpForm;
+export default AuthSignUpForm
