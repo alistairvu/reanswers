9a70cd46102f76c8b4240a3836638bc778b8f53f
phuonghaa <phuonggha39@gmail.com>
2021-04-23 20:41:15 +0700

    commit

--- frontend/src/components/question/QuestionCard.tsx
+++ frontend/src/components/question/QuestionCard.tsx
@@ -1,15 +1,21 @@
+import { Badge } from "react-bootstrap"
 import Card from "react-bootstrap/Card"
 import ReactMarkdown from "react-markdown"
 
-
-
 const QuestionCard: React.FC<QuestionInterface> = (props) => {
+  const renderTagBadges = () => {
+    const tags = props.tags
+    return tags.map(tag => <Badge>{tag}</Badge>)
+  }
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <ReactMarkdown>{props.body}</ReactMarkdown>
-
+        <div className="tags">
+            {renderTagBadges()}
+        </div>
       </Card.Body>
     </Card>
   )
