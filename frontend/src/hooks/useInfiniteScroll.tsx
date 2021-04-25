780f54eb84eb8bf2fe968de3e093ef888a981930
Alistair Vu <alistairvu@gmail.com>
2021-04-25 14:22:32 +0700

    Add useInfiniteScroll

--- frontend/src/hooks/useInfiniteScroll.tsx
+++ frontend/src/hooks/useInfiniteScroll.tsx
@@ -1,1 +1,26 @@
 import { useEffect } from "react"
+import { debounce } from "underscore"
+
+const useInfiniteScroll = (fetchNext: Function, hasNextPage: boolean) => {
+  const debouncedFetchNext = debounce(fetchNext, 1000)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight - 30
+      ) {
+        return
+      } else {
+        if (hasNextPage) {
+          debouncedFetchNext()
+        }
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [debouncedFetchNext, hasNextPage])
+}
+
+export default useInfiniteScroll
